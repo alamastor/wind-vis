2271e8b90603f84baddb420a141ea6373c68d4d8
Alistair <alistair.mckelvie@gmail.com>
2018-01-24 21:58:19 +1100

    feat: Basic animation working

---
+++ main.js
@@ -0,0 +1,47 @@
+function test() {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  particle = new Particle(ctx);
+  window.requestAnimationFrame(updateAndRender.bind(null, ctx, particle, 0));
+}
+
+
+function updateAndRender(ctx, particle, prevTime, timestamp) {
+  var dt = timestamp - prevTime;
+  console.log(dt);
+  particle.update(dt);
+
+  ctx.clearRect(0, 0, ctx.canvas.height, ctx.canvas.width);
+  particle.render(ctx);
+
+  window.requestAnimationFrame(updateAndRender.bind(null, ctx, particle, timestamp));
+}
+
+
+var Particle = function(ctx) {
+  this.ctx = ctx;
+  this.x = 0;
+  this.y = 0;
+  this.height = 10;
+  this.width = 10;
+  this.dx = 30;
+  this.dy = 30;
+};
+
+Particle.prototype.render = function(ctx) {
+  ctx.fillRect(
+    this.x - this.width / 2,
+    this.y - this.height / 2,
+    this.width,
+    this.height,
+  );
+};
+
+Particle.prototype.update = function(dt) {
+  this.x = this.x + (dt * this.dx) / 1000;
+  this.y = this.y + (dt * this.dy) / 1000;
+};
+
+
+window.onload = test;
