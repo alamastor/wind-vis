b61057a4226f8ce21e43e36bea12b8e199e73997
Alistair <alistair.mckelvie@gmail.com>
2018-02-19 13:21:39 +1100

    add app reducer tests

--- src/containers/App/tests/reducer.test.ts
+++ src/containers/App/tests/reducer.test.ts
@@ -10,4 +10,18 @@ describe('appReducer', () => {
     const expectedResult = state;
     expect(appReducer(undefined, {})).toEqual(expectedResult);
   });
+
+  it('should set display particles', () => {
+    const expectedResult = Object.assign({}, state, {displayParticles: false});
+    expect(
+      appReducer(undefined, {type: 'APP_DISPLAY_PARTICLES', display: false}),
+    ).toEqual(expectedResult);
+  });
+
+  it('should set display vectors', () => {
+    const expectedResult = Object.assign({}, state, {displayVectors: true});
+    expect(
+      appReducer(undefined, {type: 'APP_DISPLAY_VECTORS', display: true}),
+    ).toEqual(expectedResult);
+  });
 });
