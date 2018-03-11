d33c0e85ed5bddcf687a3f82d725a4e3988d089d
Alistair <alistair.mckelvie@gmail.com>
2018-03-11 15:41:43 +1100

    add responsive images

--- src/components/BackgroundMap/index.tsx
+++ src/components/BackgroundMap/index.tsx
@@ -8,12 +8,17 @@ import {
   scaleCoord,
 } from '../../Projection/Translate';
 
-const Globe = require('../../img/globe-large.png');
+const defaultGlobeImg = require(`../../img/globe-800w.png`);
+const srcSet = [200, 400, 800, 1600, 2400, 3600, 4800]
+  .map(
+    (width: number) => `${require(`../../img/globe-${width}w.png`)} ${width}w`,
+  )
+  .join(', ');
 
 const BackgroundMap = (props: {projState: ProjState}) => {
   const topLeft = transformCoord(props.projState, {lon: 0, lat: 90});
   const bottomRight = transformCoord(props.projState, {lon: 360, lat: -90});
-  const dimensions = scaleCoord(props.projState, {lon: 361, lat: 180});
+  const dimensions = scaleCoord(props.projState, {lon: 360, lat: 180});
   const width = Math.abs(dimensions.x);
   const height = Math.abs(dimensions.y);
   return (
@@ -23,10 +28,14 @@ const BackgroundMap = (props: {projState: ProjState}) => {
         width: props.projState.screen.width,
         height: props.projState.screen.height,
         overflow: 'hidden',
+        position: 'relative',
         display: 'block',
+        pointerEvents: 'none',
       })}>
       <img
-        src={Globe}
+        src={defaultGlobeImg}
+        srcSet={srcSet}
+        sizes={`${Math.round(width)}px`}
         className={style({
           position: 'absolute',
           left: topLeft.x,
@@ -37,7 +46,9 @@ const BackgroundMap = (props: {projState: ProjState}) => {
         })}
       />
       <img
-        src={Globe}
+        src={defaultGlobeImg}
+        srcSet={srcSet}
+        sizes={`${Math.round(width)}px`}
         className={style({
           position: 'absolute',
           left: topLeft.x - width,
