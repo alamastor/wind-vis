538c53c7e535584c411c32a3b073b05509ba9ca2
Alistair <alistair.mckelvie@gmail.com>
2018-03-24 20:37:32 +1100

    reduce particle alpha

--- src/components/ParticleRenderer/index.tsx
+++ src/components/ParticleRenderer/index.tsx
@@ -195,7 +195,7 @@ function getGLStateForParticles(gl: WebGLRenderingContext): GLState {
   const fragmentShaderSource = `
     varying lowp vec3 shColor;
     void main() {
-      gl_FragColor = vec4(shColor, 1) * 1.0;
+      gl_FragColor = vec4(shColor, 1) * 0.5;
     }
   `;
 
