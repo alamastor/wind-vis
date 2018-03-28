88d31337a4992bbddd080a3fc47a0fe2daa48659
Alistair <alistair.mckelvie@gmail.com>
2018-03-28 19:55:01 +1100

    fix viridis colormap

--- src/components/SpeedRenderer/index.tsx
+++ src/components/SpeedRenderer/index.tsx
@@ -59,7 +59,7 @@ export default class SpeedRenderer extends React.Component<Props, State> {
           speedData[
             181 * Math.floor(x * 360 / 512) + Math.floor(y * 181 / 512)
           ] /
-          (this.maxSpeed / 255);
+          (this.maxSpeed / 256);
       }
     }
     draw(this.getGLState(), transformedSpeedData);
@@ -106,8 +106,17 @@ function getGLStateForSpeeds(gl: WebGLRenderingContext): GLState {
     varying vec2 texCoord;
     uniform sampler2D tex;
 
+    vec3 viridis(in float i) {
+      // Convert i (intensity 0-1) to Matplotlib Viridis colormap.
+      return vec3(
+        0.279996085294 - 0.1349846921598316 * i + 2.139562241779562 * pow(i, 2.0) - 14.618561485877892 * pow(i, 3.0) + 25.097783408548356 * pow(i, 4.0) - 11.772588644921733 * pow(i, 5.0),
+        0.0010336091072 + 1.609681944005644 * i - 1.8935162112806887 * pow(i, 2.0) + 2.687992417906457 * pow(i, 3.0) - 1.6835417416062761 * pow(i, 4.0) + 0.17873836015256522 * pow(i, 5.0),
+        0.305866611269 + 2.5680305844367317 * i - 11.850371601508543 * pow(i, 2.0) + 28.67243222734909 * pow(i, 3.0) - 33.35689847617539 * pow(i, 4.0) + 13.762053354579717 * pow(i, 5.0)
+      );
+    }
+
     void main() {
-      gl_FragColor = vec4(texture2D(tex, texCoord).r, 0, 0, 1);
+      gl_FragColor = vec4(viridis(texture2D(tex, texCoord).r), 1);
     }
   `;
 
