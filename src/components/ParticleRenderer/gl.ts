0e22e2edf925a14a252549d5959434d698119392
Alistair <alistair.mckelvie@gmail.com>
2018-04-01 23:01:16 +1000

    move gl code into own modules

---
+++ src/components/ParticleRenderer/gl.ts
@@ -0,0 +1,101 @@
+import {Coord} from '../../Types';
+import {loadShader} from '../../utils/gl';
+import vertexShaderSource from './vertexshader.glsl';
+import fragmentShaderSource from './fragmentshader.glsl';
+
+export interface GLState {
+  gl: WebGLRenderingContext;
+  shaderProgram: WebGLProgram;
+  lonBuffer: WebGLBuffer;
+  latBuffer: WebGLBuffer;
+  colorBuffer: WebGLBuffer;
+}
+
+export interface Particles {
+  readonly length: number;
+  readonly lon: Float32Array;
+  readonly lat: Float32Array;
+  readonly age: Float32Array;
+}
+
+export function getGLStateForParticles(gl: WebGLRenderingContext): GLState {
+  const shaderProgram = gl.createProgram();
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  const fragmentShader = loadShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    fragmentShaderSource,
+  );
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+  gl.useProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    throw new Error(
+      'Error initializing shader program: ' +
+        gl.getProgramInfoLog(shaderProgram),
+    );
+  }
+  if (shaderProgram == null) {
+    throw new Error('shaderProgram is null');
+  }
+
+  const lonLoc = gl.getAttribLocation(shaderProgram, 'lon');
+  const lonBuffer = gl.createBuffer() as WebGLBuffer;
+  gl.bindBuffer(gl.ARRAY_BUFFER, lonBuffer);
+  gl.enableVertexAttribArray(lonLoc);
+  gl.vertexAttribPointer(lonLoc, 1, gl.FLOAT, false, 0, 0);
+
+  const latLoc = gl.getAttribLocation(shaderProgram, 'lat');
+  const latBuffer = gl.createBuffer() as WebGLBuffer;
+  gl.bindBuffer(gl.ARRAY_BUFFER, latBuffer);
+  gl.enableVertexAttribArray(latLoc);
+  gl.vertexAttribPointer(latLoc, 1, gl.FLOAT, false, 0, 0);
+
+  const colorLoc = gl.getAttribLocation(shaderProgram, 'color');
+  const colorBuffer = gl.createBuffer() as WebGLBuffer;
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.enableVertexAttribArray(colorLoc);
+  gl.vertexAttribPointer(colorLoc, 3, gl.FLOAT, false, 0, 0);
+
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+  gl.enable(gl.BLEND);
+  gl.depthMask(false);
+
+  return {gl, shaderProgram, lonBuffer, latBuffer, colorBuffer};
+}
+
+export function setViewport(glState: GLState) {
+  const {gl, shaderProgram} = glState;
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  const aspectRatioLoc = gl.getUniformLocation(shaderProgram, 'aspectRatio');
+  gl.uniform1f(aspectRatioLoc, gl.canvas.width / gl.canvas.height);
+}
+
+export function initColors(glState: GLState, colors: Float32Array) {
+  const {gl, colorBuffer} = glState;
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+}
+
+export function drawParticles(glState: GLState, positions: Particles) {
+  const {gl, lonBuffer, latBuffer} = glState;
+  gl.bindBuffer(gl.ARRAY_BUFFER, lonBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, positions.lon, gl.DYNAMIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, latBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, positions.lat, gl.DYNAMIC_DRAW);
+  gl.drawArrays(gl.POINTS, 0, positions.length);
+}
+
+export function setZoomLevel(glState: GLState, zoomLevel: number) {
+  const {gl, shaderProgram} = glState;
+  const zoomLevelLoc = gl.getUniformLocation(shaderProgram, 'zoomLevel');
+  gl.uniform1f(zoomLevelLoc, zoomLevel);
+}
+
+export function setCenterCoord(glState: GLState, centerCoord: Coord) {
+  const {gl, shaderProgram} = glState;
+  const midCoordLoc = gl.getUniformLocation(shaderProgram, 'midCoord');
+  gl.uniform2f(midCoordLoc, centerCoord.lon, centerCoord.lat);
+}
--- src/components/SpeedRenderer/index.tsx
+++ src/components/SpeedRenderer/index.tsx
@@ -2,18 +2,16 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {style} from 'typestyle';
 
-import {Coord} from '../../Types';
 import VectorField from '../../utils/fielddata/VectorField';
 import {ProjState, transformCoord, scaleCoord} from '../../utils/Projection';
-import {loadShader} from '../../utils/gl';
-import vertexShaderSource from './vertexshader.glsl';
-import fragmentShaderSource from './fragmentshader.glsl';
-
-interface GLState {
-  gl: WebGLRenderingContext;
-  shaderProgram: WebGLProgram;
-  tex: WebGLTexture;
-}
+import {
+  GLState,
+  draw,
+  getGLStateForSpeeds,
+  setCenterCoord,
+  setViewport,
+  setZoomLevel,
+} from './gl';
 
 interface Props {
   vectorField: VectorField;
@@ -83,92 +81,4 @@ export default class SpeedRenderer extends React.Component<Props, State> {
       />
     );
   }
-}
-
-function getGLStateForSpeeds(gl: WebGLRenderingContext): GLState {
-  const shaderProgram = gl.createProgram();
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-  const fragmentShader = loadShader(
-    gl,
-    gl.FRAGMENT_SHADER,
-    fragmentShaderSource,
-  );
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-  gl.useProgram(shaderProgram);
-
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    throw new Error(
-      'Error initializing shader program: ' +
-        gl.getProgramInfoLog(shaderProgram),
-    );
-  }
-  if (shaderProgram == null) {
-    throw new Error('shaderProgram is null');
-  }
-
-  // prettier-ignore
-  const points = new Float32Array([
-    -1, -1,
-    -1,  1,
-     1,  1,
-     1,  1,
-     1, -1,
-    -1, -1,
-  ]);
-
-  const pointLoc = gl.getAttribLocation(shaderProgram, 'point');
-  const pointBuffer = gl.createBuffer() as WebGLBuffer;
-  gl.bindBuffer(gl.ARRAY_BUFFER, pointBuffer);
-  gl.enableVertexAttribArray(pointLoc);
-  gl.vertexAttribPointer(pointLoc, 2, gl.FLOAT, false, 0, 0);
-  gl.bufferData(gl.ARRAY_BUFFER, points, gl.STATIC_DRAW);
-
-  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-  gl.enable(gl.BLEND);
-  gl.depthMask(false);
-
-  const tex = gl.createTexture() as WebGLTexture;
-  gl.bindTexture(gl.TEXTURE_2D, tex);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
-
-  return {gl, shaderProgram, tex};
-}
-
-function setViewport(glState: GLState) {
-  const {gl, shaderProgram} = glState;
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  const aspectRatioLoc = gl.getUniformLocation(shaderProgram, 'aspectRatio');
-  gl.uniform1f(aspectRatioLoc, gl.canvas.width / gl.canvas.height);
-}
-
-function setZoomLevel(glState: GLState, zoomLevel: number) {
-  const {gl, shaderProgram} = glState;
-  const zoomLevelLoc = gl.getUniformLocation(shaderProgram, 'zoomLevel');
-  gl.uniform1f(zoomLevelLoc, zoomLevel);
-}
-
-function setCenterCoord(glState: GLState, centerCoord: Coord) {
-  const {gl, shaderProgram} = glState;
-  const midCoordLoc = gl.getUniformLocation(shaderProgram, 'midCoord');
-  gl.uniform2f(midCoordLoc, centerCoord.lon, centerCoord.lat);
-}
-
-function draw(glState: GLState, texData: Uint8Array) {
-  const {gl, tex} = glState;
-  gl.bindTexture(gl.TEXTURE_2D, tex);
-  gl.texImage2D(
-    gl.TEXTURE_2D,
-    0,
-    gl.LUMINANCE,
-    512,
-    512,
-    0,
-    gl.LUMINANCE,
-    gl.UNSIGNED_BYTE,
-    texData,
-  );
-  gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
