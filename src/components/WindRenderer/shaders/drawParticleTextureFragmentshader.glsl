4c3a3adfe9f3185bc52548014c682e43aedcdea6
Alistair <alistair.mckelvie@gmail.com>
2018-07-11 18:26:44 +1000

    fix transforms

--- src/components/WindRenderer/shaders/drawParticleTextureFragmentshader.glsl
+++ src/components/WindRenderer/shaders/drawParticleTextureFragmentshader.glsl
@@ -17,10 +17,10 @@ void main() {
     vec2 transformed = (texCoord - 0.5)
         * (textureZoom / currentZoom)
         * vec2(min(currentAspectRatio, 2.0) / min(textureAspectRatio, 2.0),
-               min(2.0 / currentAspectRatio, 1.0) / min(textureAspectRatio, 1.0))
+               max(textureAspectRatio, 2.0) / max(currentAspectRatio, 2.0))
         + (currentCenterCoord - textureCenterCoord)
         * textureZoom / vec2(359.99, 180)
-        * 1.0 / vec2(min(textureAspectRatio / 2.0, 1.0), min(2.0 / textureAspectRatio, 1.0))
+        * vec2(2.0 / min(textureAspectRatio, 2.0), max(textureAspectRatio, 2.0) / 2.0)
         + 0.5;
     if (any(lessThan(transformed, vec2(0, 0))) || any(greaterThan(transformed, vec2(1, 1)))) {
         gl_FragColor = vec4(0, 0, 0, 0);
--- src/components/WindRenderer/shaders/drawParticleVertexshader.glsl
+++ src/components/WindRenderer/shaders/drawParticleVertexshader.glsl
@@ -11,9 +11,10 @@ void main() {
 
   float aspectRatio = canvasDimensions.x / canvasDimensions.y;
   vec2 shifted = vec2(mod(lonLat.x - centerCoord.x + 180.0, 360.0) - 180.0, lonLat.y - centerCoord.y);
-  vec2 clipSpace = 2.0 * (shifted / vec2(359.99, 180))
-                    * vec2(max(2.0 / aspectRatio, 1.0), max(aspectRatio / 2.0, 1.0))
-                    * zoomLevel;
+  vec2 clipSpace = 2.0
+                   * shifted
+                   * zoomLevel / vec2(359.99, 180)
+                   * vec2(2.0 / min(aspectRatio, 2.0), max(aspectRatio, 2.0) / 2.0);
   gl_Position = vec4(clipSpace, 0, 1);
 
   gl_PointSize = min(2.0 * zoomLevel, 5.0);
