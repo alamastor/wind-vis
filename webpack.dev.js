0750dd27ac65a70ca74bc0694b0195cfbf54e3bd
Alistair McKelvie <alistair.mckelvie@gmail.com>
2020-05-14 22:04:40 +1000

    fix eslint in webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     rules: [
       {
         enforce: 'pre',
-        test: /\.js$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         loader: 'eslint-loader',
       },
