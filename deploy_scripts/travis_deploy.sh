de35f9496d209a62188babd363730890d02a5ff9
Alistair McKelvie <alistair.mckelvie@gmail.com>
2021-11-01 10:15:59 +1100

    remove travis

--- deploy_scripts/travis_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pyenv global 3.8.1
-pip install fabric
-pip install patchwork
-openssl aes-256-cbc -K $encrypted_5a6a599da1a1_key -iv $encrypted_5a6a599da1a1_iv -in deploy_scripts/id_ed25519.enc -out deploy_scripts/id_ed25519 -d
-cd deploy_scripts && fab -H${DEPLOY_USER}@${DEPLOY_HOST} -iid_ed25519 deploy
